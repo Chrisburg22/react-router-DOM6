948b8fd4d68f9062536b3efc4c0f287110a5696d
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2023-05-06 17:32:43 -0600

    Creamos un nuevo contenxo que envolvera nuestra aplicación, utilizando el contexto para darle comunicaciión de autenticación en toda nuestra aplicación

--- src/Components/ProfilePage/index.jsx
+++ src/Components/ProfilePage/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../../context/auth";
 
 export const ProfilePage = () => {
+  const auth = useAuth();
+
   return (
-    <h1>Profile page</h1>
+    <>
+      <h1>Profile</h1>
+      <p>{auth.user? ('Welcome '+auth.user.username):'Crea un usuario'}</p>
+    </>
   );
-}
+};

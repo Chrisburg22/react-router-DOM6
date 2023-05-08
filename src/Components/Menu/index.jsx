d5bd2b41c0d80c1aff71fbab34401545dbb41a24
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2023-05-08 12:04:06 -0600

    Creando rutas publicas y privadas

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../../context/auth";
 
 const routes = [];
 routes.push({
   to: "/",
   text: "home",
+  private: false,
 });
 routes.push({
   to: "/blog",
   text: "Blog",
+  private: false,
 });
 routes.push({
   to: "/profile",
   text: "Profile",
+  private: true,
 });
 routes.push({
   to: "/login",
   text: "Login",
+  private: false,
 });
 routes.push({
   to: "/logout",
   text: "Logout",
+  private: true,
 });
 export const Menu = () => {
+  const auth = useAuth();
+
   return (
     <div>
       <ul>
         {routes.map((route) => {
+          
+          if(route.private && !auth.user) return null;
+
+          if(auth.user && route.text === "Login") return null;
+
           return (
             <li key={route.to}>
               <NavLink
